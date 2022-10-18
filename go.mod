ed68535cc5921ae4609dddf7416e7c7d3fd98dbe
Thiago Cesar Lopes <86244500+thiago2012cesar@users.noreply.github.com>
2022-10-18 13:44:26 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thiago2012cesar/codeedu-imersao-simulator
+
+go 1.16
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.6.1
+	github.com/joho/godotenv v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	kafka2 "github.com/thiago2012cesar/codeedu-imersao-simultor/application/kafka"
+	"github.com/thiago2012cesar/codeedu-imersao-simultor/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
+}
